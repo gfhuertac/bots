95b892cf74c2af45b9dc1f8e88147ff2a1599407
Gonzalo Huerta Cánepa <gonzalo@huerta.cl>
2017-05-12 10:27:36 -0300

    Added ACL to messenger-iot bot. We need to change the backend of it to a database.

--- messenger_iotbot/index.js
+++ messenger_iotbot/index.js
@@ -2,15 +2,28 @@
 const sh = require('./smart_home');
 
 // 3rd part libraries
+const Acl = require('acl');
 const Botkit = require('botkit');
 const co = require('co');
 const path = require('path');
 
 // Global variables
+const access_control = new Acl(new Acl.memoryBackend());
 const controller = Botkit.facebookbot({
   access_token: process.env.FACEBOOK_BOT_ACCESS_TOKEN,
   verify_token: process.env.FACEBOOK_BOT_VERIFY_TOKEN,
 });
+
+// loads the access control list
+access_control.addUserRoles('1385564591538464', 'family');
+access_control.allow('family', ['front_door'], 'open');
+
+// error handling method
+const handle_error = function(err, bot, message, reason) {
+  console.log(err);
+  if (bot && message)
+    bot.reply(message, 'I could not open the door for you. ' + (reason || ''));
+};
 
 // methods to be exported
 
@@ -37,14 +50,18 @@ controller.hears(['hello'], 'message_received', function(bot, message) {
 });
 
 // the bot will listen for iot commands
-controller.hears(['open', 'sesame'], 'message_received', function(bot, message) {
-  sh.fd_open(function(err) {
-    if (err) {
-      console.log(err);
-      bot.reply(message, 'I could not open the door for you');
-    }
-    else bot.reply(message, 'done!');
-  });
+controller.hears(['open'], 'message_received', function(bot, message) {
+  const user = message.user;
+  access_control.isAllowed(user, 'front_door', 'open', function(err, res){
+    if (err) handle_error(err, bot, message);
+    if(res){
+      sh.fd_open(function(err) {
+        if (err) handle_error(err, bot, message);
+        else bot.reply(message, 'done!');
+      });
+    } else
+      handle_error(err, bot, message, 'Not authorized');
+  })
 });
 
 module.exports = {
